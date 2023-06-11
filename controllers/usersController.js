48697197ddd98c0df8797d4a10c61a20d3ccdecf
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-11 16:30:08 -0300

    Asignar el rol de cliente como predeterminado

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Rol = require('../models/rol');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
@@ -102,10 +103,21 @@ module.exports = {
         }
        
         User.create(user, (err, data) => {
-            
+
+        if (err) {
+            return res.status(501).json({
+                success: false,
+                message: 'Hubo un error con el registro del usuario',
+                error: err
+            });
+        }
+
+        user.id = `${data}`;
         const token = jwt.sign({id: user.id, email: user.email}, keys.secretOrKey, {});
         user.session_token = `JWT ${token}`;
 
+        Rol.create(user.id, 3, (err, data) => {
+            
             if (err) {
                 return res.status(501).json({
                     success: false,
@@ -114,16 +126,14 @@ module.exports = {
                 });
             }
 
-            user.id = `${data}`;
-
             return res.status(201).json({
                 success: true,
                 message: 'El registro se realizo correctamente',
                 data: user
             });
-
+    
         });
-
-    },
+    });
+},
 
 }
